b7e564e50215b1a1d9c0b5e3052f608580bac66b
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-06 02:49:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,3 @@
-# require modules here
-
-def load_library
-  # code goes here
-end
-
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
+  emoticon_lib = load_library(emoticon_file)
+  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
