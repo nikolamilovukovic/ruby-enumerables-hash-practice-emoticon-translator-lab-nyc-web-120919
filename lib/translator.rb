fa2793d3f4055fd950bdfb8ee64b721f8dd9d97f
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-12-06 02:50:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,3 +1,2 @@
-  emoticon_lib = load_library(emoticon_file)
-  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
-  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
+require 'yaml'
+require 'pry'
